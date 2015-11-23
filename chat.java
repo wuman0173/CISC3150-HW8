1fe2e051b602bb8921a2d40e037d1644f0f55b79
wuman0173 <bwu0173@yahoo.com>
2015-11-23 17:20:41 -0500

    Create chat.java

---
+++ chat.java
@@ -0,0 +1,15 @@
+/*
+Benjamin Wu
+CISC 3150
+HW8
+
+Chat Program with 2 clients talk to each other, random times, with the other end receiving the message.
+Have Main Driver Class, 2 Clients, and a Server. Clients go through the Server to send/receive messages.
+
+Comments:
+
+*/
+
+public class chat{
+
+}
